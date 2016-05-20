a16fc383489fcdf648c5910b56a1e5c1e079c55a
gomez <jan.gamec@windowslive.com>
2016-05-20 20:49:18 +0200

    initial commit

---
+++ lib/Autoencoder/Autoencoder.py
@@ -0,0 +1,23 @@
+from abc import ABCMeta, abstractmethod
+## Provides a default interface for Autoencoder class
+#
+class Agent:
+  __metaclass__ = ABCMeta
+  
+  ## Training a autoencoder
+  #
+  # This method must be implemented
+  @abstractmethod
+  def train(self, *args, **kwargs): pass
+
+  ## Encoding an input and providing numpy array of features
+  #
+  # This method must be implemented
+  @abstractmethod
+  def encode(self, *args, **kwargs): pass
+
+  ## Decoding an input(features) and reconstrucing original input(image)
+  #
+  # This method must be implemented
+  @abstractmethod
+  def decode(self, *args, **kwargs): pass
