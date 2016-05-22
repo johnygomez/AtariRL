48babb2892602f4f690e9d6629d9fd721764e14d
RastoRusnak <rastorusnak93@gmail.com>
2016-05-22 16:52:26 +0100

    Folder which contains pre trained model and weights was created.

--- lib/Autoencoder/Encoder.py
+++ lib/Autoencoder/Encoder.py
@@ -6,9 +6,9 @@ from keras.models import Model
 
 class Encoder(Autoencoder):
 	def __init__(self, pretrained_weights = True):
-		self.Autoencoder = Model_from_json(open('model.json').read())
+		self.Autoencoder = Model_from_json(open('PreTrained_model/model.json').read())
 		if pretrained_weights:
-			self.Autoencoder.load_weights('NN_weights.h5')
+			self.Autoencoder.load_weights('PreTrained_model/NN_weights.h5')
 		self.Encoder = Model(input = self.Autoencoder.layers[0], output = self.Autoencoder.layers[1])
 		self.Decoder = Model(input = self.Autoencoder.layers[1], output = self.Autoencoder.layers[2])
 
